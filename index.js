e2aff77067f6e6cabc4e7930bda1456144947eb8
LovelyG7 <nia.gayle@gmail.com>
2023-06-01 15:03:33 -0400

    first-commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// // let x=2; 
+// // console.log(x);
+
+// let a=7
+// console.log(a)
+
+// let names =["jim", "james", "paul", "tyrone"];
+// console.log(names);
+
+// let seed1= "carrots";
+// console.log(seed1);
+
+// let vegetables=20;
+// console.log(vegetables);
+
+let x=12;
+let a=78;
+
+if (x ===12 && a ===78) {
+    let sum=x+a;
+    console.log(sum)
+}
