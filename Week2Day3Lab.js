7460e10ba658aeeebe89b786e4aba5f66fa531ae
LovelyG7 <nia.gayle@gmail.com>
2023-06-02 14:54:27 -0400

    Second-commit

---
+++ Week2Day3Lab.js
@@ -0,0 +1,25 @@
+let grade =100
+
+if (grade >=80) {
+  console.log("You did a good job");
+}
+
+else if (grade >= 70 && grade <80) {
+  console.log("Keep trying")
+}
+
+else if (grade >= 60 && grade < 70) {
+  console.log("ehhhh")
+}
+
+else if (grade >= 55 && grade <60) {
+  console.log(" Not too good ")
+}
+
+else if (grade <55){
+  console.log("Bad Grade But You'll Get Em Next Time")
+}
+
+else {
+  console.log("I think you're in the wrong class")
+}
--- index.js
+++ index.js
@@ -13,10 +13,68 @@
 // let vegetables=20;
 // console.log(vegetables);
 
-let x=12;
-let a=78;
+// let x=12;
+// let a=78;
 
-if (x ===12 && a ===78) {
-    let sum=x+a;
-    console.log(sum)
+// if (x ===12 && a ===78) {
+//     let sum=x+a;
+//     console.log(sum)
+// }
+
+let age=16;
+if (age>=18){
+    //do this
+    console.log("access granted");
+}
+else{
+    //do this
+    console.log("access denied");
+}
+
+let wood=25;
+if(wood >= 40) {
+    //we have enough for a treehouse
+    console.log("we have enough for a treehouse");
+}
+
+else if (wood >20 && wood <40) {
+    console.log("Build Stool");
+}
+else{
+    //too bad so sad
+    console.log("too bad so sad");
+}
+
+
+let username = "admin"
+let password = "pass614"
+
+if (username = "admin" && password == "pass614") {
+    console.log("access granted");
+}
+
+else {
+    console.log("access denied")
+}
+
+let num = 100
+if (num > 0 && num>= 100) {
+    console.log("Big");
+}
+
+else if (num >0 && num <100) {
+    console.log("Medium");
+}
+
+
+else if (num ===0){
+    console.log("ZERO");
+}
+
+else if (num <0 ) {
+    console.log("NEGATIVE");
+}
+
+else {
+    console.log("Not a Real Numer");
 }
